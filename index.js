a006af75fccd6cf2f46e410adcc1a48de751c832
Alfred <somethingfly@somethingfly.com>
2022-06-19 17:34:34 -0700

    commit tree 94684df23760a24d00185a28bf30815343ee62ae

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+*/
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(array,eventName){
+    const arrayNew = [];
+    for (let i = 0; i < array.length; i++) {
+        arrayNew.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return arrayNew;
+}
+// console.log(writeCards(gifts));
+
+function countDown(i){
+    while(i > -1){
+        console.log(i);
+        i--;
+    }
+    return
+}
+
+// countDown(10);
